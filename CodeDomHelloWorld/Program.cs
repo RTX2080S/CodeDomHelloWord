0cc54200bd05f265c8609cddfe3d1677df6d3a14
LL <nzlili2015@gmail.com>
2017-10-31 10:48:42 +0800

    Clean up imports

--- CodeDomHelloWorld/Program.cs
+++ CodeDomHelloWorld/Program.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.CSharp;
-using System;
-using System.CodeDom;
+﻿using System.CodeDom;
 using System.CodeDom.Compiler;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.CSharp;
 
 namespace CodeDomHelloWorld
 {

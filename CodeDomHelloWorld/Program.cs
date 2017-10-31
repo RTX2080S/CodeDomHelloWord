532f6fccdbccf8c2ca2e30bc80b5c9ac9e31b4e9
LL <nzlili2015@gmail.com>
2017-10-31 10:48:09 +0800

    create project files.

---
+++ CodeDomHelloWorld/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDomHelloWorld
+{
+    class Program
+    {
+        static CodeCompileUnit GetHelloWorldCodeCompileUnit()
+        {
+            CodeCompileUnit compileUnit = new CodeCompileUnit();
+            CodeNamespace myNamespace = new CodeNamespace("MyNamespace");
+            myNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            CodeTypeDeclaration myClass = new CodeTypeDeclaration("MyClass");
+            CodeEntryPointMethod start = new CodeEntryPointMethod();
+            CodeMethodInvokeExpression cs1 = new CodeMethodInvokeExpression(
+                new CodeTypeReferenceExpression("Console"),
+                "WriteLine",
+                new CodePrimitiveExpression("Hello World!"));
+            compileUnit.Namespaces.Add(myNamespace);
+            myNamespace.Types.Add(myClass);
+            myClass.Members.Add(start);
+            start.Statements.Add(cs1);
+            return compileUnit;
+        }
+
+        static void Main(string[] args)
+        {
+            var compileUnit = GetHelloWorldCodeCompileUnit();
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            using (StreamWriter sw = new StreamWriter("HelloWorld.cs", false))
+            {
+                IndentedTextWriter tw = new IndentedTextWriter(sw, " ");
+                provider.GenerateCodeFromCompileUnit(compileUnit, tw, new CodeGeneratorOptions());
+                tw.Close();
+            }
+        }
+    }
+}
